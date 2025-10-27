c469be2996aed2ef99b71f759dc371f591138ac5
Bryce? <bryce.jones2026@gbwl.org>
2025-10-27 16:35:52 +0000

    started activity

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("hello world")
+import pandas as pd
+import numpy as np
 
 
-if __name__ == "__main__":
-    main()
+df = pd.read_csv('movies_metadata_v2.csv', encoding=' iso-8859-1').dropna(axis=1, how='all')
+df.head()
+df.shape
+df.info()
+print(df.head())
+# Fill in brackets with a CONDITIONAL
+budget_df = df['budget'] > 1000000
+print(budget_df.shape) #5
+# Fill in the parameters for (values, index)
+budget_lookup = pd.Series()
+
