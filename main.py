97bef71fd95c924df697c1c70c9332b6e80e2e28
Bryce? <bryce.jones2026@gbwl.org>
2025-10-28 19:08:54 +0000

    A and B

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ df.shape
 df.info()
 print(df.head())
 # Fill in brackets with a CONDITIONAL
-budget_df = df['budget'] > 1000000
+budget_df = df[df['budget'] > 1000000]
 print(budget_df.shape) #5
 # Fill in the parameters for (values, index)
-budget_lookup = pd.Series()
+budget_lookup = pd.Series(budget_df['budget'].values, index=budget_df['title'])
+print(budget_lookup)
+print(budget_lookup['Jumanji']) #65 Mil
+
+# First define the condition to be checked
+condition = (budget_lookup.index >= 'A Bag of Hammers') & (budget_lookup.index <= 'Byzantium')
+# Pull rows conditionally
+budget_lookup_A_B = budget_lookup[condition]
 
